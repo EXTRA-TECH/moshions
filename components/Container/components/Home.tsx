5f9366b526d2b1cf3abad21317e753885699e571
j4l13n <juliushirwa@gmail.com>
2021-12-23 17:01:48 +0200

    add bag to home page

--- components/Container/components/Home.tsx
+++ components/Container/components/Home.tsx
@@ -1,4 +1,16 @@
+import Bag from "../../Bag";
+import Link from 'next/link'
+import { useAuth, useCart } from "@saleor/sdk";
+import {useQuery} from '@apollo/react-hooks'
+
 const Home = () => {
+  const { items } = useCart();
+
+  // const {data, loading, error} = useQuery(CATEGORIES)
+
+  const cartItemsQuantity =
+    (items &&
+      items.reduce((prevVal, currVal) => prevVal + currVal.quantity, 0));
   return (
     <>
       <section className="u-align-center u-clearfix u-section-1" id="sec-d02b">
@@ -11,18 +23,19 @@ const Home = () => {
                   <img src="/images/MoShiOns_LoGo_White.svg" className="u-logo-image u-logo-image-1" data-image-width="92" />
                 </a>
                 <span className="u-enable-responsive u-icon u-icon-circle u-text-white u-icon-1">
-                  <svg className="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 51.997 51.997" ><use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#svg-1702"></use></svg>
+                  {/* <svg className="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 51.997 51.997" ><use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#svg-1702"></use></svg>
                   <svg version="1.1" id="svg-1702" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 51.997 51.997" xmlSpace="preserve" className="u-svg-content"><path d="M51.911,16.242C51.152,7.888,45.239,1.827,37.839,1.827c-4.93,0-9.444,2.653-11.984,6.905
 	c-2.517-4.307-6.846-6.906-11.697-6.906c-7.399,0-13.313,6.061-14.071,14.415c-0.06,0.369-0.306,2.311,0.442,5.478
 	c1.078,4.568,3.568,8.723,7.199,12.013l18.115,16.439l18.426-16.438c3.631-3.291,6.121-7.445,7.199-12.014
-	C52.216,18.553,51.97,16.611,51.911,16.242z"></path><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
+	C52.216,18.553,51.97,16.611,51.911,16.242z"></path><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg> */}
                 </span>
                 <span className="u-enable-responsive u-icon u-icon-circle u-text-white u-icon-2">
-                  <svg className="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 51.997 51.997" ><use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#svg-bcb3"></use></svg>
+                  {/* <svg className="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 51.997 51.997" ><use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#svg-bcb3"></use></svg>
                   <svg version="1.1" id="svg-bcb3" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 51.997 51.997" xmlSpace="preserve" className="u-svg-content"><path d="M51.911,16.242C51.152,7.888,45.239,1.827,37.839,1.827c-4.93,0-9.444,2.653-11.984,6.905
 	c-2.517-4.307-6.846-6.906-11.697-6.906c-7.399,0-13.313,6.061-14.071,14.415c-0.06,0.369-0.306,2.311,0.442,5.478
 	c1.078,4.568,3.568,8.723,7.199,12.013l18.115,16.439l18.426-16.438c3.631-3.291,6.121-7.445,7.199-12.014
-	C52.216,18.553,51.97,16.611,51.911,16.242z"></path><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
+	C52.216,18.553,51.97,16.611,51.911,16.242z"></path><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg> */}
+                  <Bag cartItemsQuantity={cartItemsQuantity} />
                 </span>
                 <nav className="u-menu u-menu-dropdown u-offcanvas u-menu-1">
                   <div className="menu-collapse" style={{fontWeight: 700}}>
